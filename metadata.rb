5a1b87a1971f100da47e3fa979358ebaa9dd6953
Eric G. Wolfe <wolfe21@marshall.edu>
2012-05-22 01:50:22 -0400

    foodcritic cannot infer name of cookbook with self-dependencies

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "wolfe21@marshall.edu"
 license          "Apache 2.0"
 description      "Installs and configures nfs, and NFS exports"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name "nfs"
 version          "0.2.2"
 recipe "nfs", "Installs and configures nfs client components"
 recipe "nfs::server", "Installs and configures nfs server components"
