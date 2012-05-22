37d14344e86bf5c3827c5c21bcc04de006a548fd
Eric G. Wolfe <wolfe21@marshall.edu>
2012-05-22 01:32:45 -0400

    add recipe metadata

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,10 @@ license          "Apache 2.0"
 description      "Installs and configures nfs, and NFS exports"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.2"
+recipe "nfs", "Installs and configures nfs client components"
+recipe "nfs::server", "Installs and configures nfs server components"
+recipe "nfs::exports", "Templates the exports file from attribute or LWRP"
+recipe "nfs::undo", "Undo both default and server recipes"
 
 %w{ ubuntu debian redhat centos fedora scientific amazon oracle }.each do |os|
   supports os
