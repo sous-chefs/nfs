c5752a9eb427c4b02e05c08c9292b96ac1cdd5a4
Eric G. Wolfe <eric.wolfe@gmail.com>
2014-03-20 23:17:28 -0400

    Don't timeout on me rubygems/Travis

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 maintainer       'Eric G. Wolfe'
-maintainer_email 'wolfe21@marshall.edu'
+maintainer_email 'eric.wolfe@gmail.com'
 license          'Apache 2.0'
 description      'Installs and configures nfs, and NFS exports'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
