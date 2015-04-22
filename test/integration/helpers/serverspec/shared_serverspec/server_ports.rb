6f358ddf0573b835f51c4b43b1a4d5534f7a1088
Joe Rocklin <joe.rocklin@gmail.com>
2015-04-22 09:51:07 -0400

    Add more port checks, and fix a typo in lock daemon check

--- test/integration/helpers/serverspec/shared_serverspec/server_ports.rb
+++ test/integration/helpers/serverspec/shared_serverspec/server_ports.rb
@@ -1,6 +1,26 @@
 shared_examples "server_ports" do
-  describe "NFS Server Ports" do
-    describe port(32678) do
+  context "Portmap daemon" do
+    describe port(111) do
+      it { should be_listening.with('tcp') }
+      it { should be_listening.with('udp') }
+    end
+  end
+
+  context "Stat daemon" do
+    describe port(32765) do
+      it { should be_listening.with('tcp') }
+    end
+  end
+
+  context "Mount daemon" do
+    describe port(32767) do
+      it { should be_listening.with('tcp') }
+      it { should be_listening.with('udp') }
+    end
+  end
+
+  context "Lock daemon" do
+    describe port(32768) do
       it { should be_listening.with('tcp') }
       it { should be_listening.with('udp') }
     end
