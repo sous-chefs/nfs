c0f9924d10f05accf73ed2bc5230260bdfc4adc1
Joe Rocklin <joseph.rocklin@siemens.com>
2015-06-03 09:43:44 -0400

    Remove 'contains' matcher in issue 46 tests
    There is conflicting documentation on the longevity of the 'contains' matcher. Since the test didn't work in ubuntu with the contains matcher anyway, this switches it over to just using regex checks.

--- test/integration/helpers/serverspec/shared/issues/issue-gh46.rb
+++ test/integration/helpers/serverspec/shared/issues/issue-gh46.rb
@@ -9,10 +9,10 @@ shared_examples "issues::gh46" do
     context "Unrelated shares are not stairstepping anonuid/anongid" do
       describe command("egrep -v '/tmp/share[0-9] 127.0.0.1\\(rw,sync,root_squash,(anonuid=[0-9]+,anongid=[0-9]+){2,}\\)' /etc/exports") do
         its(:exit_status) { should eq 0 }
-        its(:stdout) { should_not contain('anonuid=1001').after('share2') }
-        its(:stdout) { should_not contain('anongid=1001').after('share2') }
-        its(:stdout) { should_not contain('anonuid=1002').after('share3') }
-        its(:stdout) { should_not contain('anongid=1002').after('share3') }
+        its(:stdout) { should_not match(/^\/tmp\/share2[.]*anonuid=1001/) }
+        its(:stdout) { should_not match(/^\/tmp\/share2[.]*anongid=1001/) }
+        its(:stdout) { should_not match(/^\/tmp\/share3[.]*anonuid=1002/) }
+        its(:stdout) { should_not match(/^\/tmp\/share3[.]*anongid=1002/) }
       end
     end
   end
