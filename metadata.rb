67910654f837ed65de19504d604b1aeb459aa830
Eric G. Wolfe <eric.wolfe@gmail.com>
2016-04-20 10:19:05 -0400

    Add source_url and issues_url

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ description 'Installs and configures NFS, and NFS exports'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name 'nfs'
 version '2.2.6'
+source_url 'https://github.com/atomic-penguin/cookbook-nfs'
+issues_url 'https://github.com/atomic-penguin/cookbook-nfs/issues'
 
 %w(ubuntu debian redhat centos fedora scientific amazon oracle sles freebsd).each do |os|
   supports os
